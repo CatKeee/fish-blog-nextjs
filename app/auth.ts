8150735cf28aaccb0a61ade3dd2110e14a33609d
JXDN <2749143951@qq.com>
2024-07-12 11:30:39 +0800

    fix: build error

--- app/auth.ts
+++ app/auth.ts
@@ -1,15 +1,3 @@
-// import NextAuth from "next-auth";
-// import Github from "next-auth/providers/github";
-
-// export const { handlers, auth, signIn, signOut } = NextAuth({
-//   providers: [
-//     Github({
-//       clientId: process.env.GITHUB_CLIENT_ID as string,
-//       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-//     }),
-//   ],
-// });
-
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
